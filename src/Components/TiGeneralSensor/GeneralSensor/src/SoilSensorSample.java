72f2957b555a12235ae5102931e7510588e01dde
lemon <lemon.yang@tijos.net>
2017-12-08 14:27:03 +0800

    add general sensor samples

---
+++ src/Components/TiGeneralSensor/GeneralSensor/src/SoilSensorSample.java
@@ -0,0 +1,76 @@
+
+import tijos.framework.sensor.general.ITiGeneralSensorEventListener;
+import tijos.framework.sensor.general.TiGeneralSensor;
+import tijos.framework.devicecenter.TiADC;
+import tijos.framework.devicecenter.TiGPIO;
+
+//土壤湿度传感器 
+class SoilSensorEventListener implements ITiGeneralSensorEventListener {
+
+	@Override
+	public void onThresholdNotify(TiGeneralSensor arg0) {
+		System.out.println("GPIO Event");
+		
+	}
+}
+
+
+public class SoilSensorSample {
+
+	public static void main(String[] args) {
+		/*
+		 * 定义使用的TiGPIO port
+		 * 定义使用的TiADC port
+		 */
+		int adcPort0 = 0;
+		int gpioPort0 = 0;
+		/*
+		 * 定义所使用的gpioPin
+		 * */
+		int gpioPin0 = 0;
+		/*
+		 * 资源分配，
+		 * 将gpioPort与gpioPin0分配给TiGPIO对象gpio0
+		 * 将adcPort0分配给TiADC对象adc0
+		 */
+		TiGPIO gpio0 = TiGPIO.open(gpioPort0, gpioPin0);
+		TiADC adc0 = TiADC.open(adcPort0);
+		/*
+		 * 资源绑定，
+		 * 创建TiGeneralSensor对象并将gpioPort、gpioPortPin和adcPort与其绑定
+		 * Pin0<---->D0
+		 * ADC <---->A0
+		 */	
+		TiGeneralSensor soilSensor = new TiGeneralSensor(gpio0, gpioPin0, adc0);
+		
+		/*
+		 * 资源使用，
+		 * 创建事件监听对象并设置事件监听
+		 * 在事件监听中处理事件逻辑
+		 */			
+		SoundSensorEventListener lc = new SoundSensorEventListener();
+		soilSensor.setEventListener(lc);
+	
+		while(true) {
+			try {
+				
+				double vol = soilSensor.getAnalogOutput();
+				System.out.println("Votagel: " + vol);
+				
+				if(soilSensor.getDigitalOutput() == 1)
+				{
+					System.out.println("DO high");
+				}
+				else
+				{
+					System.out.println("DO low");
+				}
+				
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}		
+		}	
+
+	}
+
+}
---
+++ src/Components/TiGeneralSensor/GeneralSensor/src/TouchButtonSample.java
@@ -0,0 +1,65 @@
+
+
+import tijos.framework.devicecenter.TiGPIO;
+import tijos.framework.sensor.button.TiButton;
+import tijos.framework.sensor.button.ITiButtonEventListener;
+
+//触摸按键
+
+class OneButtonEventListener implements ITiButtonEventListener {
+
+    /**
+     * 按键按下事件处理
+     */
+    public void onPressed(TiButton arg0) {
+        System.out.println("onPressed"+"  time(us):"+arg0.getEventTime());      
+    }
+
+    /**
+     * 按键释放事件处理
+     */
+    public void onReleased(TiButton arg0) {
+        System.out.println("onReleased"+"  time(us):"+arg0.getEventTime());     
+    }   
+}
+
+public class TouchButtonSample{
+    /**
+     * 程序入口，由TiJOS调用
+     * @param args 入口参数， TiJOS中一直等于null
+     */
+    public static void main(String[] args) {
+        /*
+         * 定义使用的TiGPIO port
+         */
+        int gpioPort0 = 0;
+        /*
+         * 定义使用的TiGPIO pin
+         */
+        int gpioPin0 = 0;
+
+        /*
+         * 资源分配，
+         * 将gpioPort0与gpioPin0分配给TiGPIO对象gpio0
+         */         
+        TiGPIO gpio0 = TiGPIO.open(gpioPort0, gpioPin0);
+        /*
+         * 资源绑定，
+         * 创建TiButton对象buttonS1并将gpio0和gpioPin0与其绑定
+         */ 
+        TiButton buttonS1 = new TiButton(gpio0, gpioPin0);
+        
+        OneButtonEventListener lc = new OneButtonEventListener();
+        buttonS1.setEventListener(lc);   
+        
+        while(true) {
+			try {
+	
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}		
+		}	
+    }
+}
+
+        
