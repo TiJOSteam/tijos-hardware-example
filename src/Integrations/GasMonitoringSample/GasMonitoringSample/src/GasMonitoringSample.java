d91e25e64fb30f43fbd8b1b8465a339cde6a3915
andy <andy.yang@tijos.net>
2018-05-22 17:30:07 +0800

    DHT11采集周期调整为2.5秒。

--- src/Integrations/GasMonitoringSample/GasMonitoringSample/src/GasMonitoringSample.java
+++ src/Integrations/GasMonitoringSample/GasMonitoringSample/src/GasMonitoringSample.java
@@ -123,8 +123,8 @@ class HumitureMonitor extends Thread {
 					this._oled.print(1, 5, humi + "% ");
 					humiLast = humi;
 				}
-				//延迟2秒再次采集温湿度
-				Delay.msDelay(2000);
+				//延迟2.5秒再次采集温湿度
+				Delay.msDelay(2500);
         	}
         	catch(IOException e) {
         		e.printStackTrace();
